3a134e5e476ed8e840677b4dbbca782641c1ad60
afelix997 <afelix2013@gmail.com>
2022-05-25 06:53:45 -0700

    new

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,11 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
-};
-
+let answer = []
+if (arrayToSearchThrough.indexOf(valueToFind) !== -1) {
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {  
+        if (arrayToSearchThrough[i]== valueToFind){
+            answer.push(i)
+        }} return answer;
+    }else {return null}
+}
