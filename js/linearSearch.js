dadd1f3e65d3f88410157b60c81f09b2b8216979
afelix997 <afelix2013@gmail.com>
2022-05-25 17:02:26 -0700

    new

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,11 +4,20 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    let answer = undefined
+    if (arrayToSearchThrough.indexOf(valueToFind) !== -1) {
+        for (let i = 0; i < arrayToSearchThrough.length; i++) {  
+            if (arrayToSearchThrough[i]== valueToFind){
+                answer = i 
+              break
+            }} return answer;
+    }}
+    
+exports.linearSearchGlobal= function(valueToFind, arrayToSearchThrough){
 let answer = []
 if (arrayToSearchThrough.indexOf(valueToFind) !== -1) {
     for (let i = 0; i < arrayToSearchThrough.length; i++) {  
         if (arrayToSearchThrough[i]== valueToFind){
             answer.push(i)
         }} return answer;
-    }else {return null}
-}
+    }else {return null}}
